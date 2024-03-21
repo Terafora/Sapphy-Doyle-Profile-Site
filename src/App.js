abcc7afba5f4a045a0be96269a0f20fbc5f8869d
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-21 10:04:48 +0000

    Update app with routing paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import History from './components/History';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <NavBar />
+          <Switch>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/contact" element={<Contact />} />
+          </Switch>
+        </Route>
+      </ Routes>
+    </BrowserRouter>
   );
 }
 
