ba902fe65e3f88c50a1880f77d32430262f1d5e4
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-21 10:05:32 +0000

    Setup routes in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav>
+      <h2>Sapphy Doyle</h2>
+      <ul>
+        <li><link to="/">Home</link></li>
+        <li><link to="/about">About</link></li>
+        <li><link to="/skills">Skills</link></li>
+        <li><link to="/history">History</link></li>
+        <li><link to="/contact">Contact</link></li>
+      </ul>
+    </nav>
   );
 }
