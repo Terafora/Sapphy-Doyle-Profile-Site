7f34c636f1da41d28398033936a6ec650182372f
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-21 12:15:35 +0000

    Fix error with Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav>
       <h2>Sapphy Doyle</h2>
       <ul>
-        <li><link to="/">Home</link></li>
-        <li><link to="/about">About</link></li>
-        <li><link to="/skills">Skills</link></li>
-        <li><link to="/history">History</link></li>
-        <li><link to="/contact">Contact</link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
+        <li><Link to="/history">History</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
