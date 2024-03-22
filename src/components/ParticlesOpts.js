c72a7036dfcb261a6e132f332818c50532210dfe
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-22 10:41:18 +0000

    Add settings to ParticlesOpts component

---
+++ src/components/ParticlesOpts.js
@@ -0,0 +1,113 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesComponent = (props) => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(() => ({
+    background: {
+      color: {
+        value: "#000000",
+      },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: ["#ff0000", // Red
+        "#00ff00", // Green
+        "#0000ff", // Blue
+        "#ffff00", // Yellow
+        "#ff00ff", // Magenta
+        "#00ffff", // Cyan
+        "#ff8000", // Orange
+        "#8000ff", // Purple
+        "#80ff00", // Lime
+        "#0080ff", // Light Blue
+        "#ff0080", // Pink
+        "#00ff80",],
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 3,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle", // Change shape to circle for bubble effect
+      },
+      size: {
+        random: true,
+        value: 8,
+      },
+    },
+    detectRetina: true,
+  }), []);
+
+  return <Particles id={props.id} init={particlesLoaded} options={options} />;
+};
+
+export default ParticlesComponent;
