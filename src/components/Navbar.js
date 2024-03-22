f3dda44e614afb693d56fa5579a9bd1cfe68e2ad
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-22 15:22:56 +0000

    Correct layout in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,20 @@ import './NavBar.css';
 
 export default function NavBar() {
   return (
-  <Navbar expand="lg" className="navbar-sect">
-  <Container>
-    <Navbar.Brand><Link className="links" to="/">Sapphy Doyle</Link></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav>
-        <Nav.Link><Link className="links" to="/">Home</Link></Nav.Link>
-        <Nav.Link><Link className="links" to="/about">About</Link></Nav.Link>
-        <Nav.Link><Link className="links" to="/skills">Skills</Link></Nav.Link>
-        <Nav.Link><Link className="links" to="/history">History</Link></Nav.Link>
-        <Nav.Link><Link className="links" to="/contact">Contact</Link></Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-  </Navbar>
+    <Navbar expand="lg" className="navbar-sect">
+    <Container>
+      <Navbar.Brand><Link className="links" to="/">Sapphy Doyle</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
+          <Nav.Link><Link className="links" to="/">Home</Link></Nav.Link>
+          <Nav.Link><Link className="links" to="/about">About</Link></Nav.Link>
+          <Nav.Link><Link className="links" to="/skills">Skills</Link></Nav.Link>
+          <Nav.Link><Link className="links" to="/history">History</Link></Nav.Link>
+          <Nav.Link><Link className="links" to="/contact">Contact</Link></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+    </Navbar>
   );
 }
