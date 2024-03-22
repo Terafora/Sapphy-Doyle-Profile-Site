c8df0f932d9a6152f346492d7e2f82f71e43bd03
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-22 16:41:55 +0000

    Make CV downloadable and indicate download succesful

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload, faCheck } from '@fortawesome/free-solid-svg-icons';
 import './Home.css';
+import SapphyDoyleCV from '../assets/Sapphy-Doyle-CV.pdf';
 
 export default function Home() {
+
+  const [showTick, setShowTick] = useState(false);
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = SapphyDoyleCV;
+    link.download = 'Sapphy-Doyle-CV.pdf';
+    link.click();
+
+    setShowTick(true);
+    setTimeout(() => {
+      setShowTick(false);
+    }, 5000);
+  };
+
   return (
     <div className="home container d-flex align-items-center justify-content-center">
       <div className="row">
@@ -15,6 +33,15 @@ export default function Home() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at accumsan nibh. Curabitur consequat vestibulum eros, suscipit luctus tellus congue non. Pellentesque a dui mauris. Donec pharetra at nunc vel condimentum. Phasellus non sodales erat, non auctor magna. Nam nec ante non nibh pretium pharetra. Nunc a ante sit amet leo imperdiet porttitor. Praesent arcu mi, sollicitudin sit amet egestas quis, rhoncus sagittis eros. Donec elit arcu, ultrices in tempor quis, mattis quis nisi. Mauris sit amet blandit dui. Sed scelerisque ut velit quis condimentum. Vestibulum a lorem erat. Proin at diam eu diam convallis scelerisque. Praesent quam diam, pulvinar nec pulvinar id, rhoncus ac erat. Nullam quam enim, tristique sed sollicitudin a, sollicitudin ut nulla. Nam vulputate suscipit dui, in auctor sem rhoncus eu.
             Morbi sit amet dui quam. Quisque in augue faucibus, fringilla ipsum ut, dapibus nibh. Cras hendrerit lobortis risus at aliquet. Sed eu quam enim. Phasellus ultrices ligula eget turpis dapibus, auctor lobortis ipsum vestibulum. Nam pulvinar nisi et mollis accumsan. Aenean fringilla metus quam, et porta metus egestas et. In vel neque quam. Curabitur id ipsum ut elit pulvinar porttitor. In quis rutrum diam, eget gravida massa. Donec non felis diam.
           </p>
+          {showTick ? (
+            <h3 className="text-white">
+              <FontAwesomeIcon icon={faCheck} /> Download complete
+            </h3>
+          ) : (
+            <h3 className="text-white resume-download" onClick={handleDownload}>
+              <FontAwesomeIcon icon={faDownload} /> Download my resume
+            </h3>
+          )}
         </div>
       </div>
     </div>
