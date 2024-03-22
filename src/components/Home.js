03f303db3fd2077d593fa0b5df0d880e61673a76
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-22 16:57:44 +0000

    Centre Sapphy's name

--- src/components/Home.js
+++ src/components/Home.js
@@ -37,7 +37,7 @@ export default function Home() {
           <img src="https://fakeimg.pl/400x600?font=bebas" alt="placeholder" />
         </div>
         <div className="col-lg-6 glassbg">
-          <h1 className="text-white">Sapphy Doyle</h1>
+          <h1 className="text-white text-center">Sapphy Doyle</h1>
           <br></br>
           <p className="text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at accumsan nibh. Curabitur consequat vestibulum eros, suscipit luctus tellus congue non. Pellentesque a dui mauris. Donec pharetra at nunc vel condimentum. Phasellus non sodales erat, non auctor magna. Nam nec ante non nibh pretium pharetra. Nunc a ante sit amet leo imperdiet porttitor. Praesent arcu mi, sollicitudin sit amet egestas quis, rhoncus sagittis eros. Donec elit arcu, ultrices in tempor quis, mattis quis nisi. Mauris sit amet blandit dui. Sed scelerisque ut velit quis condimentum. Vestibulum a lorem erat. Proin at diam eu diam convallis scelerisque. Praesent quam diam, pulvinar nec pulvinar id, rhoncus ac erat. Nullam quam enim, tristique sed sollicitudin a, sollicitudin ut nulla. Nam vulputate suscipit dui, in auctor sem rhoncus eu.
