f7f4085aca700f95990a3ed587736dff1be02964
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-03-25 17:51:57 +0000

    Add styling corrections to Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -135,17 +135,17 @@ export default function Skills() {
                     <div className="col-lg-11 py-5 px-5 d-flex justify-content-center align-items-center d-none d-lg-block">
                         {renderSkill()}
                     </div>
-                    <div className="col-md-12  py-5 px-5 d-lg-none">
+                    <div className="col-md-12 py-5 px-5 d-lg-none mt-0">
                         <div>{skillOne}</div>
-                        <div className="my-5">{skillTwo}</div>
-                        <div className="my-5">{skillThree}</div>
-                        <div className="my-5">{skillFour}</div>
-                        <div className="my-5">{skillFive}</div>
-                        <div className="my-5">{skillSix}</div>
-                        <div className="my-5">{skillSeven}</div>
-                        <div className="my-5">{skillEight}</div>
-                        <div className="my-5">{skillNine}</div>
-                        <div className="my-5">{skillTen}</div>
+                        <div className="my-3">{skillTwo}</div>
+                        <div className="my-3">{skillThree}</div>
+                        <div className="my-3">{skillFour}</div>
+                        <div className="my-3">{skillFive}</div>
+                        <div className="my-3">{skillSix}</div>
+                        <div className="my-3">{skillSeven}</div>
+                        <div className="my-3">{skillEight}</div>
+                        <div className="my-3">{skillNine}</div>
+                        <div className="mt-3">{skillTen}</div>
                     </div>
                 </div>
             </div>
