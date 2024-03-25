5bbce2be76cc7471425fcb1c26308b5fc6b5deae
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-03-25 13:20:54 +0000

    Add some place holder variables for future

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,34 @@
 import React, {useState} from 'react';
 
 export default function Skills() {
+
+
+
+    const skillOne = (
+        <div>
+            <h3 className="text-white">Skill One</h3>
+            <p className="text-white">Description of Skill One</p>
+        </div>
+    );
+
+    const skillTwo = (
+        <div>
+            <h3 className="text-white">Skill Two</h3>
+            <p className="text-white">Description of Skill Two</p>
+        </div>
+    );
+
+    const skillThree = (
+        <div>
+            <h3 className="text-white">Skill Three</h3>
+            <p className="text-white">Description of Skill Three</p>
+        </div>
+    );
+
     return (
         <div>
-        <h1>Skills</h1>
+        <h1 className="text-white text-center">Skills</h1>
+
         </div>
     );
 }
