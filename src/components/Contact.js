8afb67ec8cdbb6e4daa28f08676278fddd39a5c0
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-26 15:40:31 +0000

    Open email input in new window on clicking email displayed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,13 @@ import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import './Contact.css';
 
 export default function Contact() {
+
+  const emailAddress = "sapphydoyle@gmail.com";
+
+  const handleEmailClick = () => {
+    window.open(`mailto:${emailAddress}`, "_blank");
+  }
+
   return (
     <div className="container-fluid">
       <div className="container d-flex align-items-center justify-content-center contact-info">
@@ -11,7 +18,7 @@ export default function Contact() {
           <div className="col-lg-12 text-center py-5 px-5 glassbg">
             <h1 className="text-white">Contact</h1>
             <br></br>
-            <h3 className="text-white mt-3"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon> Email: sapphydoyle@gmail.com</h3>
+            <h3 className="text-white mt-3 email" onClick={handleEmailClick}><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon> Email: {emailAddress}</h3>
             <h3 className="text-white mt-3"><FontAwesomeIcon icon={faPhone}></FontAwesomeIcon> Phone: 07708 363700</h3>
           </div>
         </div>
