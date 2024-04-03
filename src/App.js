b2fb27d5b6cd831b5d7d4f51aadf199232b2ace7
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-04-03 14:52:12 +0100

    Fix issues alongside Mathew C. for deployment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ParticlesComponent from './components/ParticlesOpts';
-import AnimatedRoutes from './components/AnimatedRoutes';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import TopNav from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ParticlesComponent from "./components/ParticlesOpts";
+import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <NavBar />
-        <ParticlesComponent id="tsparticles" />
-        <AnimatedRoutes />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <TopNav />
+                <ParticlesComponent id="tsparticles" />
+                <AnimatedRoutes />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import './NavBar.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./NavBar.css";
 
-export default function NavBar() {
-  const location = useLocation();
+export default function TopNav() {
+    const location = useLocation();
 
-  return (
-    <Navbar expand="lg" className="navbar-sect">
-      <Container>
-        <Navbar.Brand><Link className="links text-white" to="/">Sapphy Doyle</Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link><Link className={`links ${location.pathname === '/' ? 'text-white' : ''}`} to="/">Home</Link></Nav.Link>
-            <Nav.Link><Link className={`links ${location.pathname === '/about' ? 'text-white' : ''}`} to="/about">About</Link></Nav.Link>
-            <Nav.Link><Link className={`links ${location.pathname === '/skills' ? 'text-white' : ''}`} to="/skills">Skills</Link></Nav.Link>
-            <Nav.Link><Link className={`links ${location.pathname === '/history' ? 'text-white' : ''}`} to="/history">History</Link></Nav.Link>
-            <Nav.Link><Link className={`links ${location.pathname === '/contact' ? 'text-white' : ''}`} to="/contact">Contact</Link></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar expand="lg" className="navbar-sect">
+            <Container>
+                <Navbar.Brand>
+                    <Link className="links text-white" to="/">
+                        Sapphy Doyle
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link>
+                            <Link className={`links ${location.pathname === "/" ? "text-white" : ""}`} to="/">
+                                Home
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link className={`links ${location.pathname === "/about" ? "text-white" : ""}`} to="/about">
+                                About
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link
+                                className={`links ${location.pathname === "/skills" ? "text-white" : ""}`}
+                                to="/skills"
+                            >
+                                Skills
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link
+                                className={`links ${location.pathname === "/history" ? "text-white" : ""}`}
+                                to="/history"
+                            >
+                                History
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link
+                                className={`links ${location.pathname === "/contact" ? "text-white" : ""}`}
+                                to="/contact"
+                            >
+                                Contact
+                            </Link>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
