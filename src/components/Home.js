1de1685a8ea76ccb84cc2cee63bcfeb42981d2be
Charlotte Stone <charlie.stone649@gmail.com>
2024-04-15 12:03:29 +0100

    Update content on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faCheck } from '@fortawesome/free-solid-svg-icons';
 import './Home.css';
 import SapphyDoyleCV from '../assets/Sapphy-Doyle-CV.pdf';
+import image from "../assets/images/SapphyDoyleProfilePic.jpg";
 import { motion } from 'framer-motion';
 import { pageVariants, pageTransition } from './AnimationVariations';
 
@@ -38,15 +39,14 @@ export default function Home() {
       <div className="home container d-flex align-items-center justify-content-center">
         <div className="row">
           <div className="col-12 col-lg-6 my-5 d-flex justify-content-center">
-            <img src="https://fakeimg.pl/400x600?font=bebas" className="rounded" alt="placeholder" />
+            <img src={image} className="rounded home-pic" alt="Sapphy Doyle" />
           </div>
           <div className="col-lg-6 glassbg d-flex align-items-center">
             <div className="text-center">
               <h1 className="text-white">Sapphy Doyle</h1>
               <br></br>
               <p className="text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at accumsan nibh. Curabitur consequat vestibulum eros, suscipit luctus tellus congue non. Pellentesque a dui mauris. Donec pharetra at nunc vel condimentum. Phasellus non sodales erat, non auctor magna. Nam nec ante non nibh pretium pharetra. Nunc a ante sit amet leo imperdiet porttitor. Praesent arcu mi, sollicitudin sit amet egestas quis, rhoncus sagittis eros. Donec elit arcu, ultrices in tempor quis, mattis quis nisi. Mauris sit amet blandit dui. Sed scelerisque ut velit quis condimentum. Vestibulum a lorem erat. Proin at diam eu diam convallis scelerisque. Praesent quam diam, pulvinar nec pulvinar id, rhoncus ac erat. Nullam quam enim, tristique sed sollicitudin a, sollicitudin ut nulla. Nam vulputate suscipit dui, in auctor sem rhoncus eu.
-                Morbi sit amet dui quam. Quisque in augue faucibus, fringilla ipsum ut, dapibus nibh. Cras hendrerit lobortis risus at aliquet. Sed eu quam enim. Phasellus ultrices ligula eget turpis dapibus, auctor lobortis ipsum vestibulum. Nam pulvinar nisi et mollis accumsan. Aenean fringilla metus quam, et porta metus egestas et. In vel neque quam. Curabitur id ipsum ut elit pulvinar porttitor. In quis rutrum diam, eget gravida massa. Donec non felis diam.
+              I am an extremely driven individual with long experience and well developed skills in client experience roles of myriad variety. With recent experience in the medical-optical; service and sales field. I have well established tech skills and a strong affinity for both hardware and software from long time hobby and professional uses of computers and adjacent tech. My ambition is now to grow this well cultivated collection of skills with my personal drive and hunger for learning to pursue and grow a career in tech.
               </p>
               {showTick ? (
                 <h3 className="mt-5 resume-download">
